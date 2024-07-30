e9e53411909c7e087c42a3b4a4ad15e60ef190a6
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-07-30 14:24:10 -0300

    feat: add menu path

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Menu from "../pages/Menu/Menu";
 
 function RoutesApp() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Menu />
+            </>
+          }
+        />
       </Routes>
     </>
   );
