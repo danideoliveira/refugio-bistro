b5a39a2c3ba5d1c9977274b4fc50da93a3ecd11c
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-07-30 14:25:37 -0300

    feat: add menu images

--- client/src/components/Images/Images.tsx
+++ client/src/components/Images/Images.tsx
@@ -1,5 +1,25 @@
 import homeImage from "../../assets/img/homeImage.jpg";
 
+// Carousel
+import meal1 from "../../assets/img/meal1.jpg";
+import meal2 from "../../assets/img/meal2.jpg";
+import meal3 from "../../assets/img/meal3.jpg";
+import snack1 from "../../assets/img/snack1.jpg";
+import snack2 from "../../assets/img/snack2.jpg";
+import snack3 from "../../assets/img/snack3.jpg";
+import dessert1 from "../../assets/img/dessert1.jpg";
+import dessert2 from "../../assets/img/dessert2.jpg";
+import dessert3 from "../../assets/img/dessert3.jpg";
+
 export const images = {
   homeImage,
+  meal1,
+  meal2,
+  meal3,
+  snack1,
+  snack2,
+  snack3,
+  dessert1,
+  dessert2,
+  dessert3,
 };
