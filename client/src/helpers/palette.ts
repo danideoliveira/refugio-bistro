7980c0612c15bacb6456459c7e7bb32fe3704d11
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-07-31 18:45:28 -0300

    feat: add form colors

--- client/src/helpers/palette.ts
+++ client/src/helpers/palette.ts
@@ -18,12 +18,23 @@ const colors = {
     v1: "#0E0E0E",
     v2: "#1C1C1C",
   },
+
+  red: {
+    v1: "#FF0000",
+  },
 };
 
 export const palette = {
   header_background: colors.black.v1,
   header_link_color: colors.beige.v3,
   header_link_background: colors.green.v1,
+
+  form_background: colors.white.v1,
+  form_title_color: colors.green.v2,
+  form_text_color: colors.green.v2,
+  form_button_color: colors.white.v1,
+  form_button_background: colors.green.v2,
+  form_error: colors.red.v1,
 
   home_title_color: colors.beige.v3,
   home_text_color: colors.beige.v3,
