ebadf651ed678e587608c56872bc1caf2a1358c7
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-07-31 18:45:07 -0300

    feat: add login and register routes

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu";
 import Location from "../pages/Location/Location";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 function RoutesApp() {
   return (
@@ -17,6 +19,8 @@ function RoutesApp() {
             </>
           }
         />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
