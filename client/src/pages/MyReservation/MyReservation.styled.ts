ce9b085dcd3bcbd292890af6a12629d9bdf640d8
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-07 14:43:50 -0300

    feat: create my reservation page

---
+++ client/src/pages/MyReservation/MyReservation.styled.ts
@@ -0,0 +1,206 @@
+import styled from "styled-components";
+import { images } from "../../components/Images/Images";
+import { setFlexbox } from "../../helpers/mixins";
+import { palette } from "../../helpers/palette";
+import { Link } from "react-router-dom";
+
+export const Container = styled.section`
+  ${setFlexbox("center", "center", "column")}
+  width: 100%;
+  height: 100vh;
+  background-color: aliceblue;
+  background-image: ${`url(${images.profileBackground})`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+  gap: 2rem;
+
+  form {
+    max-width: 60rem;
+  }
+
+  .box-inputs {
+    ${setFlexbox("center", "center", "column")}
+    gap: 3rem;
+  }
+
+  .box-buttons {
+    ${setFlexbox("center", "center", "row")}
+    gap: 2rem;
+  }
+
+  .logout-button {
+    padding: 0.5rem 1rem;
+    font-size: 1.6rem;
+    text-decoration: none;
+    transition: 0.3s all ease;
+    font-weight: 500;
+    border-radius: 5px;
+    background: darkred;
+    color: ${palette.header_link_color};
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  padding: 0.5rem 1rem;
+  font-size: 1.5rem;
+  text-decoration: none;
+  transition: 0.3s all ease;
+  background-color: ${palette.reservation_button_background};
+  color: ${palette.reservation_button_color};
+  font-weight: 500;
+  border-radius: 5px;
+`;
+
+export const Reservations = styled.div`
+  ${setFlexbox("space-evenly", "center", "column")}
+  width: 100%;
+  padding: 5rem 0;
+  gap: 5rem;
+  max-width: 65rem;
+  background-color: ${palette.form_background};
+  border-radius: 5px;
+
+  h2 {
+    color: ${palette.form_title_color};
+    font-size: 2.6rem;
+  }
+
+  label {
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: ${palette.form_text_color};
+  }
+`;
+
+export const Table = styled.table`
+  border: none;
+  margin: 0;
+  padding: 0;
+  table-layout: fixed;
+  border-collapse: collapse;
+
+  & .action-row button {
+    ${setFlexbox("center", "center", "row")}
+    font-size: 1.3rem;
+    gap: 0.5rem;
+
+    svg {
+      font-size: 2rem;
+    }
+  }
+
+  & thead tr {
+    border-radius: 50%;
+    color: ${palette.reservation_background};
+    background-color: ${palette.reservation_title_color};
+  }
+
+  & tbody td {
+    padding: 2rem 1rem;
+  }
+
+  & caption {
+    font-size: 1.5rem;
+    margin: 0.5rem 0 0.75rem;
+  }
+
+  & tr {
+    background-color: #f1f1f1;
+    border: 3px solid #fff;
+    padding: 0.35rem;
+    font-size: 1.4rem;
+  }
+
+  & th {
+    padding: 1rem 1rem;
+    text-align: center;
+    font-size: 1.4rem;
+    text-transform: uppercase;
+  }
+
+  & td {
+    text-align: center;
+  }
+
+  & td .action {
+    border: 0;
+    padding: 5px;
+    border-radius: 4px;
+    display: inline-block;
+    margin-right: 3px;
+  }
+
+  & td .action svg {
+    vertical-align: middle;
+  }
+
+  & td .badge {
+    padding: 3px;
+    border-radius: 3px;
+    color: #fff;
+  }
+
+  button {
+    ${setFlexbox("center", "center", "row")}
+    height: 3rem;
+    border: none;
+    border-radius: 5px;
+    color: ${palette.form_button_color};
+    font-weight: 600;
+    transition: 0.3s all ease;
+    cursor: pointer;
+    width: auto;
+    font-size: 1.5rem;
+    padding: 1rem 1rem;
+    background-color: darkred;
+
+    &:hover {
+      filter: contrast(1.5);
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    & caption {
+      font-size: 1.3rem;
+    }
+
+    & thead {
+      border: none;
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      width: 1px;
+    }
+
+    & tr {
+      border: 3px solid #ddd;
+      border-radius: 5px;
+      display: block;
+      margin-bottom: 0.6rem;
+    }
+
+    & td {
+      border-bottom: 1px solid #ddd;
+      display: block;
+      font-size: 1.4rem;
+      text-align: right;
+    }
+
+    & td::before {
+      content: attr(data-label);
+      float: left;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+`;
---
+++ client/src/pages/MyReservation/MyReservation.tsx
@@ -0,0 +1,160 @@
+import { useContext, useState } from "react";
+import {
+  Container,
+  Reservations,
+  StyledLink,
+  Table,
+} from "./MyReservation.styled";
+import { AuthContext } from "../../contexts/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { toast } from "react-toastify";
+import { TiDelete } from "react-icons/ti";
+import Modal from "../../components/Modal/Modal";
+import { IoIosArrowBack } from "react-icons/io";
+import { InfoSquare, ReviewGrid } from "../Reservation/Reservation.styled";
+
+function MyReservation() {
+  const { user, setUser }: any = useContext(AuthContext);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [chosenReservation, setChosenReservation] = useState<any>("");
+  let ordenedReservations = user?.reservations;
+
+  if (user?.reservations.length > 1) {
+    ordenedReservations =
+      user?.reservations[0].date > user?.reservations[1].date
+        ? [user?.reservations[1], user?.reservations[0]]
+        : [user?.reservations[0], user?.reservations[1]];
+  }
+
+  async function handleDeleteRow(date: string) {
+    try {
+      const updatedReservations = user.reservations.filter(
+        (reservation: any) => reservation.date !== date,
+      );
+
+      const docRef = doc(db, "users", user.uid);
+      await updateDoc(docRef, { reservations: updatedReservations });
+
+      const updatedUser = {
+        name: user.name,
+        email: user.email,
+        uid: user.uid,
+        reservations: updatedReservations,
+      };
+      localStorage.setItem("@currentUser", JSON.stringify(updatedUser));
+
+      setUser(updatedUser);
+      setShowModal(!showModal);
+      toast.info(`Sua reserva para o dia ${date} foi cancelada!`);
+    } catch (error) {
+      console.error("Erro ao deletar reserva: ", error);
+      toast.error("Erro ao deletar reserva.");
+    }
+  }
+
+  function handleShowModal(reservation: any) {
+    setChosenReservation(reservation);
+    setShowModal(!showModal);
+  }
+
+  return (
+    <Container>
+      <Reservations>
+        <h2>Minhas Reservas</h2>
+
+        {user?.reservations.length > 0 ? (
+          <Table>
+            <thead>
+              <tr>
+                <th>Data</th>
+                <th>Horário</th>
+                <th>Pessoas</th>
+                <th>Ambiente</th>
+                <th>Unidade</th>
+                <th>#</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {user?.reservations &&
+                ordenedReservations.map((currentReservation: any) => (
+                  <tr key={currentReservation.date}>
+                    <td data-label="Data da Reserva">
+                      {currentReservation.date}
+                    </td>
+                    <td data-label="Horário">{currentReservation.hour}</td>
+                    <td data-label="Pessoas">{currentReservation.people}</td>
+                    <td data-label="Ambiente">{currentReservation.place}</td>
+                    <td data-label="Unidade">{currentReservation.location}</td>
+                    <td className="action-row">
+                      <button
+                        type="button"
+                        onClick={() => handleShowModal(currentReservation)}
+                      >
+                        <TiDelete /> Cancelar reserva
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        ) : (
+          <label>Você não possui nenhuma reserva.</label>
+        )}
+        {user?.reservations.length < 2 && (
+          <StyledLink to="/reservation">+ Nova Reserva</StyledLink>
+        )}
+      </Reservations>
+      {showModal && (
+        <Modal>
+          <button className="modal-close" onClick={() => setShowModal(false)}>
+            <IoIosArrowBack />
+          </button>
+          <h2>Deseja cancelar a sua reserva?</h2>
+
+          <ReviewGrid className="modal-info">
+            <InfoSquare>
+              <label>Data</label>
+              <p>{chosenReservation.date}</p>
+            </InfoSquare>
+
+            <InfoSquare>
+              <label>Horário</label>
+              <p>{chosenReservation.hour}</p>
+            </InfoSquare>
+
+            <InfoSquare>
+              <label>Pessoas</label>
+              <p>{chosenReservation.people}</p>
+            </InfoSquare>
+
+            <InfoSquare>
+              <label>Ambiente</label>
+              <p>{chosenReservation.place}</p>
+            </InfoSquare>
+
+            <InfoSquare>
+              <label>Unidade</label>
+              <p>{chosenReservation.location}</p>
+            </InfoSquare>
+          </ReviewGrid>
+
+          <div className="button-box">
+            <button
+              className="modal-button-delete"
+              onClick={async () =>
+                await handleDeleteRow(chosenReservation.date)
+              }
+            >
+              <TiDelete />
+              Cancelar reserva
+            </button>
+          </div>
+        </Modal>
+      )}
+    </Container>
+  );
+}
+
+export default MyReservation;
