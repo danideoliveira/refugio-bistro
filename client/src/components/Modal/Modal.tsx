1d6ea0fb09252dfe2496c7f0ec69b406df53d978
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-09 17:02:50 -0300

    feat: add modal condition and classname

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,16 +1,30 @@
 import { ReactNode } from "react";
 import { Container } from "./Modal.styled";
+import { IoIosArrowBack } from "react-icons/io";
 
 interface ModalProps {
   children: ReactNode;
+  condition: boolean;
+  setCondition: React.Dispatch<React.SetStateAction<boolean>>;
+  className: string;
 }
 
-function Modal({ children }: ModalProps) {
+function Modal({ children, condition, setCondition, className }: ModalProps) {
   return (
     <>
-      <Container>
-        <div className="modal-box">{children}</div>
-      </Container>
+      {condition && (
+        <Container className={className}>
+          <div className="modal-box">
+            <button
+              className="modal-close"
+              onClick={() => setCondition(!condition)}
+            >
+              <IoIosArrowBack />
+            </button>
+            {children}
+          </div>
+        </Container>
+      )}
     </>
   );
 }
