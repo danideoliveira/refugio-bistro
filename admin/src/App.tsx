f022e66984dbe812747983164dfab3642a9ad914
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-12 20:40:30 -0300

    feat: create admin folder

---
+++ admin/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <div>
+        <h1>Olá</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ admin/src/components/Images/Images.tsx
@@ -0,0 +1,5 @@
+import logo from "../../assets/img/logo.jpg";
+
+export const images = {
+  logo,
+};
