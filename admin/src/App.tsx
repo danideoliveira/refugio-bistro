5d1bb6245867c39864cd2a98730def2058f38b56
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-15 21:07:15 -0300

    feat: update app

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,9 +1,36 @@
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header/Header";
+import AdminProvider from "./contexts/authAdmin";
+import GlobalStyles from "./globalStyles";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes/Routes";
+
 function App(): JSX.Element {
   return (
     <>
-      <div>
-        <h1>Olá</h1>
-      </div>
+      <BrowserRouter>
+        <AdminProvider>
+          <ToastContainer
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            style={{
+              fontSize: "1.5rem",
+            }}
+          />
+
+          <GlobalStyles />
+          <RoutesApp />
+        </AdminProvider>
+      </BrowserRouter>
     </>
   );
 }
