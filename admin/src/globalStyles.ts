ace522109df354a6d2121c6886a9af896f6136e7
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-19 14:07:15 -0300

    feat: set background

--- admin/src/globalStyles.ts
+++ admin/src/globalStyles.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import { setFlexbox } from "./helpers/mixins";
+import { images } from "./components/Images/Images";
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -9,13 +10,17 @@ const GlobalStyles = createGlobalStyle`
     font-family: "Source Sans 3", sans-serif;
     /* outline: 1px solid rebeccapurple; */
   }
-
+  
   .container {
     ${setFlexbox("center", "center", "row")}
   }
-
+  
   html {
+    background: #0e0e0e;
     font-size: 62.5%;
+    background-image: ${`url(${images.loginBackground})`};
+    background-size: cover;
+    background-repeat: no-repeat;
 
     @media screen and (max-width: 780px) {
       font-size: 60%;
