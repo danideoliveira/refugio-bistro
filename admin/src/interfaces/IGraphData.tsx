d34f7f68639b4b4539446f39d397cf0c7595ea2d
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2024-08-26 21:04:30 -0300

    feat: creating interfaces folder

---
+++ admin/src/interfaces/IGraphData.tsx
@@ -0,0 +1,8 @@
+export interface IGraphData {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: any[];
+    backgroundColor: string[];
+  }[];
+}
---
+++ admin/src/interfaces/IReservation.tsx
@@ -0,0 +1,14 @@
+export interface IReservation {
+  uid?: string;
+  cpf: string;
+  name: string;
+  phone: string;
+  email: string;
+  date: string;
+  hour: string;
+  people: string;
+  place: string;
+  location: string;
+  moment: string;
+  status: string;
+}
